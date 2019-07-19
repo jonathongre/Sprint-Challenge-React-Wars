6daac849edbab4aa06908fca9617370c5728b779
Jonathon Green <ericg9811@gmail.com>
2019-07-19 13:20:14 -0500

    ran load test in cypress

--- starwars/cypress/integration/sample_spec.js
+++ starwars/cypress/integration/sample_spec.js
@@ -1,5 +1,5 @@
 describe('My First Test', function() {
-  it('Does not do much!', function() {
-    expect(true).to.equal(true)
+  it('successfully loads', function() {
+    cy.visit('http://localhost:3000')
   })
 })
